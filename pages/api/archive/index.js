4c6d8ba2f0c76d13dd007f213a2e6e63f282144f
davide.antinori <antinoridavide@outlook.it>
2021-02-09 19:38:57 +0100

    09/02/2021 | Archive api finally working

--- pages/api/archive/index.js
+++ pages/api/archive/index.js
@@ -1,9 +1,9 @@
-import fs from 'fs'
+import fs from 'fs'//pacchetto usato per docx files
 import path from 'path'
 import slash from 'slash'
-import { PdfReader } from "pdfreader"
-import mammoth from 'mammoth'
-import Blob from "cross-blob"
+import { PdfReader } from "pdfreader"  //pacchetto usato per pdf files
+import mammoth from 'mammoth' //pacchetto usato per pdf files
+import WordExtractor from "word-extractor" //pacchetto usato per doc files
 
 
 export default async (req, res) => {
@@ -35,60 +35,40 @@ export default async (req, res) => {
     }
   })()
 
-  const containerPromise = new Promise((resolveContainer, rejectContainer) => {
-    const analyzedFiles = []
-    filesToAnalyze.forEach(fileObj => {
-      const pdf = fileObj.fullpath.includes(".pdf")
-      const singleFilePromise = new Promise((resolveSingle, rejectSingle) => {
-        if (pdf) {
-          let pdfContentArray = []
-          new PdfReader().parseFileItems(fileObj.fullpath, function (err, item) {
-            if (err) {
-              rejectSingle(err)
-            } else if (!item) {
-              resolveSingle(pdfContentArray.join(" ")) //La callback è stata effettuata su tutto il pdf, esco.
-            } else if (item.text) {
-              pdfContentArray.push(item.text) //Per ogni frammento del pdf, pusho.
-            }
-          })
-        } else {
-          const docContent = fs.readFileSync(fileObj.fullpath, 'utf8')
-          const docBuffer = fs.readFileSync(fileObj.fullpath)
-          //console.log("\n\n\n ||||| docBuffer stringed:", docBuffer.toString().substr(0, 4000), "\n\n\n")
-          const docBlob = new Blob(docBuffer)
-          console.log("\n\n\n ||||| AT CREATION - docBlob:", docBlob, "\n\n\n")
-          console.log("\n\n\n ||||| AT CREATION - blob size:", docBlob.size, "\n\n\n")
-          resolveSingle({
-            fullpath: fileObj.fullpath,
-            linuxfullpath: fileObj.linuxfullpath,
-            filename: fileObj.filename,
-            relativepath: fileObj.relativepath,
-            linuxpath: fileObj.linuxpath,
-            content: docContent,
-            blob: docBlob
-          })
-        }
-      }).then((singleResult) => {
-        //[PDF] singleResult ha l'aspetto di oggetto con prop numerata, quindi lo adatto
-        if (singleResult.blob) {
-          //console.log("\n\n\n ||||| AFTER THEN - singleResult without content ", { ...singleResult, content: "" })
-          //console.log("\n\n\n ||||| AFTER THEN - singleResult.blob of " + fileObj.filename + ":", singleResult.blob, "\n\n\n")
-          //console.log("\n\n\n ||||| AFTER THEN - singleResult.blob.size of " + fileObj.filename + ":", singleResult.blob.size, "\n\n\n")
-        }
-        if (Object.keys(singleResult).includes('0')) {
-          analyzedFiles.push({
-            fullpath: fileObj.fullpath,
-            linuxfullpath: fileObj.linuxfullpath,
-            filename: fileObj.filename,
-            relativepath: fileObj.relativepath,
-            linuxpath: fileObj.linuxpath,
-            content: Object.values(singleResult).join(""),
-            blob: singleResult.blob
-          })
-        } else {
-          //[DOC/DOCX] singleResult ha un aspetto normale, tranne per il content che è una copia di sè. Quindi il content diventa la prop['content']
+  //containerResult Promise starts pending
+  const containerResult = await new Promise((resolveContainer, rejectContainer) => {
+    try {
+      const analyzedFiles = []
+      filesToAnalyze.forEach(async fileObj => {
+        const pdf = fileObj.fullpath.includes(".pdf")
+        const docx = fileObj.fullpath.includes(".docx")
+        const doc = fileObj.fullpath.includes(".doc")
 
-          if (fileObj.filename.includes("provafileesterno")) {
+        //singleResult Promise starts pending
+        const singleResult = await new Promise((resolveSingle, rejectSingle) => {
+          if (pdf) { //[Pdf procedure] (PdfReader + manual array push)
+            const getPdfContent = async () => {
+              const pdfContentArray = []
+              await new PdfReader().parseFileItems(fileObj.fullpath, function (err, item) {
+                if (err) return rejectSingle(err) //rejecting singleResult Promise
+                if (!item) { //Condizione d'uscita da parseFileItems()
+                  return resolveSingle({ //resolving singleResult Promise
+                    fullpath: fileObj.fullpath,
+                    linuxfullpath: fileObj.linuxfullpath,
+                    filename: fileObj.filename,
+                    relativepath: fileObj.relativepath,
+                    linuxpath: fileObj.linuxpath,
+                    content: pdfContentArray.join(" ")
+                  })
+                }
+                if (item.text) { //Per ogni frammento del pdf, pusho in pdfContentArray.
+                  pdfContentArray.push(item.text)
+                  return true
+                }
+              })
+            }
+            getPdfContent()
+          } else if (docx) {//[Docx procedure] (mammoth)
             const options = {}
             mammoth.convertToHtml({ path: 'public\\' + fileObj.relativepath }, options).then((mammothResult) => {
               if (mammothResult.messages.length > 0) {
@@ -96,46 +76,79 @@ export default async (req, res) => {
                   console.log("\n\n Errors:", mammothResult.messages[x], '\n\n')
                 }
               }
-              //console.log("\n\n\n\n ||||| mammothResult.value: ", mammothResult.value, "\n\n\n\n")
+              return resolveSingle({ //resolving singleResult Promise
+                fullpath: fileObj.fullpath,
+                linuxfullpath: fileObj.linuxfullpath,
+                filename: fileObj.filename,
+                relativepath: fileObj.relativepath,
+                linuxpath: fileObj.linuxpath,
+                content: mammothResult.value
+              })
             })
+          } else if (doc) { //[Doc procedure] (WordExtractor)
+            const getDocContent = async () => {
+              const docExtractor = new WordExtractor()
+              const extractedContent = docExtractor.extract('public\\' + fileObj.relativepath)
+              await extractedContent.then(function (doc) {
+                resolveSingle({ //resolving singleResult Promise
+                  fullpath: fileObj.fullpath,
+                  linuxfullpath: fileObj.linuxfullpath,
+                  filename: fileObj.filename,
+                  relativepath: fileObj.relativepath,
+                  linuxpath: fileObj.linuxpath,
+                  content: JSON.stringify(doc.getBody())
+                })
+              })
+            }
+            getDocContent()
+          } else {
+            rejectSingle("File is not pdf, docx or doc!") //rejecting singleResult Promise
           }
-          analyzedFiles.push({
-            fullpath: fileObj.fullpath,
-            filename: fileObj.filename,
-            relativepath: fileObj.relativepath,
-            linuxpath: fileObj.linuxpath,
-            content: singleResult.content,
-            blob: singleResult.blob
-          })
-        }
-        if (analyzedFiles.length === filesToAnalyze.length) {
-          resolveContainer(analyzedFiles)
-        }
-      })
-    })
+        }).then(singleResult => {
+          return singleResult
+        })
+        //singleResult Promise resolved/rejected
 
+        await analyzedFiles.push({
+          fullpath: fileObj.fullpath,
+          filename: fileObj.filename,
+          relativepath: fileObj.relativepath,
+          linuxpath: fileObj.linuxpath,
+          content: singleResult.content
+        })
+
+        if (analyzedFiles.length === filesToAnalyze.length) {
+          resolveContainer(analyzedFiles) //resolving containerResult Promise
+        }
+
+      })
+    } catch (errContainer) {
+      rejectContainer(errContainer) //rejecting containerResult Promise
+    }
   }).then((containerResult) => {
-    const filteredDocs = containerResult.filter(d => {
+    return containerResult
+  })
+  //containerResult Promise resolved/rejected
+
+  const filteredDocs = containerResult.filter(d => {
+    if (d.content) {
       //Eventuali affinamenti del filtro andranno qui
       const cleanContent = d.content.replace(/[^\w\s]/gi, '').toLowerCase()
-      if (cleanContent.includes(searchterms.replace(/[^\w\s]/gi, '').toLowerCase())) {
-        //console.log("Content tradotto dal binario: ", d.content.replace(/[^\w\s]/gi, '').substr(0, 4000))
-        //console.log("Blob del .doc file: ", d.blob)
-      }
       return cleanContent.includes(searchterms.replace(/[^\w\s]/gi, '').toLowerCase())
-    })
-    console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n Pulizia Console \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-    if (filteredDocs.length > 0) {
-      console.log("About to send data to client | blob size:", filteredDocs[0].blob.size)
-      return res.status(200).json({ //Success - Trovato qualcosa per i searchterms immessi
-        success: true,
-        data: { filteredDocs: filteredDocs }
-      })
     } else {
-      return res.status(200).json({ //Success - ma nessun risultatp per i searchterms immessi
-        success: true,
-        data: { filteredDocs: [] }
-      })
+      return false
     }
   })
+
+  if (filteredDocs.length > 0) {
+    return res.status(200).json({ //Success - Trovato qualcosa per i searchterms immessi, e nessun errore.
+      success: true,
+      data: { filteredDocs: filteredDocs }
+    })
+  } else {
+    return res.status(200).json({ //Success - ma nessun risultatp per i searchterms immessi, ma nessun errore.
+      success: true,
+      data: { filteredDocs: [] }
+    })
+  }
 }
---
+++ pages/api/archive/indexBACKUP.js
@@ -0,0 +1,173 @@
+import fs from 'fs'//pacchetto usato per docx files
+import path from 'path'
+import slash from 'slash'
+import { PdfReader } from "pdfreader"  //pacchetto usato per pdf files
+import mammoth from 'mammoth' //pacchetto usato per pdf files
+import Blob from "cross-blob"
+import StreamZip from 'node-stream-zip' //pacchetto usato per doc files
+
+
+export default async (req, res) => {
+  if (!req.query.searchterms) return res.status(400).json({ success: false, error: "Missing searchterms!" })
+  const searchterms = req.query.searchterms
+  const filesToAnalyze = []
+
+  //funzione che estrae i path precisi di ogni file all'interno della dir archive
+  function* getFiles(dir) {
+    const dirents = fs.readdirSync(dir, { withFileTypes: true })
+    for (const dirent of dirents) {
+      const fullpath = path.resolve(dir, dirent.name)
+      if (dirent.isDirectory()) {
+        yield* getFiles(fullpath)
+      } else {
+        yield {
+          fullpath: fullpath,
+          linuxfullpath: slash(fullpath),
+          relativepath: fullpath.split("public\\")[1],
+          linuxpath: slash(fullpath.split("public\\")[1]),
+          filename: dirent.name
+        }
+      }
+    }
+  }
+  (() => {
+    for (const f of getFiles('public/archive')) {
+      filesToAnalyze.push(f)
+    }
+  })()
+
+  const containerPromise = new Promise((resolveContainer, rejectContainer) => {
+    try {
+      const analyzedFiles = []
+      filesToAnalyze.forEach(fileObj => {
+        const pdf = fileObj.fullpath.includes(".pdf")
+        const singleFilePromise = new Promise((resolveSingle, rejectSingle) => {
+          try {
+            if (pdf) { //[Pdf procedure]
+              let pdfContentArray = []
+              new PdfReader().parseFileItems(fileObj.fullpath, function (err, item) {
+                if (err) {
+                  rejectSingle(err)
+                } else if (!item) {
+                  resolveSingle(pdfContentArray.join(" ")) //La callback è stata effettuata su tutto il pdf, esco.
+                } else if (item.text) {
+                  pdfContentArray.push(item.text) //Per ogni frammento del pdf, pusho.
+                }
+              })
+            } else {
+              const docx = fileObj.fullpath.includes(".docx")
+              if (docx) { //[Docxx procedure]
+                const docxContent = fs.readFileSync(fileObj.fullpath, 'utf8')
+                const docxBuffer = fs.readFileSync(fileObj.fullpath)
+                const docxBlob = new Blob(docxBuffer)
+                resolveSingle({
+                  fullpath: fileObj.fullpath,
+                  linuxfullpath: fileObj.linuxfullpath,
+                  filename: fileObj.filename,
+                  relativepath: fileObj.relativepath,
+                  linuxpath: fileObj.linuxpath,
+                  content: docxContent,
+                  blob: docxBlob
+                })
+              } else { //[Doc procedure]
+                const docProcedure = new Promise((resolveDoc, rejectDoc) => {
+                  try {
+                    const zip = new StreamZip.async({ file: fileObj.fullpath })
+                    setTimeout(() => resolveDoc(zip), 1000)
+                  } catch (errDoc) {
+                    rejectDoc(errDoc)
+                  }
+                }).then((zipResult) => {
+                  console.log("\n\n zipResult:", zipResult, "\n\n")
+                  const entriesCount = zipResult.entriesCount
+                  return entriesCount
+                  console.log("\n\n entriesCount:", entriesCount, "\n\n")
+                  resolveSingle({
+                    fullpath: fileObj.fullpath,
+                    linuxfullpath: fileObj.linuxfullpath,
+                    filename: fileObj.filename,
+                    relativepath: fileObj.relativepath,
+                    linuxpath: fileObj.linuxpath,
+                    content: entriesCount,
+                    blob: []
+                  })
+                })
+              }
+            }
+          } catch (errSingleFile) {
+            rejectSingle(errSingleFile)
+          }
+        }).then((singleResult) => {
+
+          //console.log(`Entries read: ${entriesCount}`)
+          //[PDF] singleResult ha l'aspetto di oggetto con prop numerata, quindi lo adatto
+          if (singleResult.blob) {
+            //console.log("\n\n\n ||||| AFTER THEN - singleResult without content ", { ...singleResult, content: "" })
+            //console.log("\n\n\n ||||| AFTER THEN - singleResult.blob of " + fileObj.filename + ":", singleResult.blob, "\n\n\n")
+            //console.log("\n\n\n ||||| AFTER THEN - singleResult.blob.size of " + fileObj.filename + ":", singleResult.blob.size, "\n\n\n")
+          }
+          if (Object.keys(singleResult).includes('0')) {
+            analyzedFiles.push({
+              fullpath: fileObj.fullpath,
+              linuxfullpath: fileObj.linuxfullpath,
+              filename: fileObj.filename,
+              relativepath: fileObj.relativepath,
+              linuxpath: fileObj.linuxpath,
+              content: Object.values(singleResult).join(""),
+              blob: singleResult.blob
+            })
+          } else {
+            //[DOC/DOCX] singleResult ha un aspetto normale, tranne per il content che è una copia di sè. Quindi il content diventa la prop['content']
+
+            if (fileObj.filename.includes("provafileesterno")) {
+              const options = {}
+              mammoth.convertToHtml({ path: 'public\\' + fileObj.relativepath }, options).then((mammothResult) => {
+                if (mammothResult.messages.length > 0) {
+                  for (let x; x < mammothResult.messages.length; x++) {
+                    console.log("\n\n Errors:", mammothResult.messages[x], '\n\n')
+                  }
+                }
+                //console.log("\n\n\n\n ||||| mammothResult.value: ", mammothResult.value, "\n\n\n\n")
+              })
+            }
+            analyzedFiles.push({
+              fullpath: fileObj.fullpath,
+              filename: fileObj.filename,
+              relativepath: fileObj.relativepath,
+              linuxpath: fileObj.linuxpath,
+              content: singleResult.content,
+              blob: singleResult.blob
+            })
+          }
+          if (analyzedFiles.length === filesToAnalyze.length) {
+            resolveContainer(analyzedFiles)
+          }
+        })
+      })
+    } catch (errContainer) {
+      rejectContainer(errContainer)
+    }
+  }).then((containerResult) => {
+    const filteredDocs = containerResult.filter(d => {
+      //Eventuali affinamenti del filtro andranno qui
+      const cleanContent = d.content.replace(/[^\w\s]/gi, '').toLowerCase()
+      if (cleanContent.includes(searchterms.replace(/[^\w\s]/gi, '').toLowerCase())) {
+        //console.log("Content tradotto dal binario: ", d.content.replace(/[^\w\s]/gi, '').substr(0, 4000))
+        //console.log("Blob del .doc file: ", d.blob)
+      }
+      return cleanContent.includes(searchterms.replace(/[^\w\s]/gi, '').toLowerCase())
+    })
+    console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n Pulizia Console \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    if (filteredDocs.length > 0) {
+      return res.status(200).json({ //Success - Trovato qualcosa per i searchterms immessi
+        success: true,
+        data: { filteredDocs: filteredDocs }
+      })
+    } else {
+      return res.status(200).json({ //Success - ma nessun risultatp per i searchterms immessi
+        success: true,
+        data: { filteredDocs: [] }
+      })
+    }
+  })
+}
