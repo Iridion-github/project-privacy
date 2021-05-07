ee82caf04b6d2e5205404479362a591d2a95fc15
iridion-github <antinoridavide@outlook.it>
2021-05-07 19:51:16 +0200

    Gitignore issue fixed

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/articoli":["static\u002Fchunks\u002Fpages\u002Farticoli.js"],sortedPages:["\u002F_app","\u002F_error","\u002Farticoli"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
