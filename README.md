2c04045d7c6c03081e0640a5f3a02f35ef4e6c22
iridion-github <antinoridavide@outlook.it>
2021-05-20 20:10:14 +0200

    20/05/2021 | Even more quizArea styling

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 ## [ToDo]
-- [Styling] 
+- 
 ## [Checkpoint]
+- Restyle del timer in areaQuiz. Muy faigo ora, ma cmq migliorabile.
+- Invitato Ale nel cluster mongoDB, ora ha totale controllo sul db GM_consulting.
 - Inserimento Quiz completato
 ## [Deployment]
 - Vercel: https://vercel.com/iridion-github/project-privacy
 ## [Richieste/Domande_per_Gaetano]
 - Attualmente la breadcrumbs bar appare solo se ci si trova in un articolo/recensione, chiedere se preferisce che sia presente anche direttamente nella sezione esterna di articoli e reviews.
 ## [Note]
-- Prepararsi a migrare il DB o a creare un user che consenta ad Ale l'accesso NON AL CLUSTER ma a quel db.
+- 
 ## [Known_Issues/Bugs]
-- [Articoli] Selezionare un topic fa partire il loader e non lo blocca.
-- [AreaQuiz] Quando si va oltre i 60 minuti, il timer si fotte.
+- [AreaQuiz] Lo scadere del tempo non ha reali implicazioni. Implementare.
 - [All] Cambiare lingua e clickare un link riporta alla lingua originale (ita).
 - [Articoli+RecensioniBibliografiche] Il filtro per argomenti fa partire lo spinner loader ad oltranza.
 ## Implementazioni Possibili/Utili e Migliorie
--- components/areaQuiz/QuizTimer.js
+++ components/areaQuiz/QuizTimer.js
@@ -4,11 +4,54 @@ import {
   Card
 } from 'react-bootstrap'
 import Timer from 'react-compound-timer'
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 
 export const QuizTimer = function (props) {
+
+  const getFormattedTimer = ({ remainingTime }) => {
+    const hours = Math.floor(remainingTime / 3600)
+    const minutes = Math.floor((remainingTime % 3600) / 60)
+    const seconds = remainingTime % 60
+
+    return `${hours}:${minutes}:${seconds}`
+  }
+
   return (
     <Row className="w-100 ml-0 mr-0 mt-2 justify-content-center align-items-center text-center">
       <Col offset={8} md={4}>
+        <>
+          <Row className="w-100 ml-0 mr-0 justify-content-center align-items-center text-center">
+            <Col className="text-center p-0">
+              <strong>Tempo Rimasto:</strong>
+            </Col>
+          </Row>
+          <Row className="w-100 ml-0 mr-0 p-0  justify-content-center align-items-center text-center">
+            <Col md={{ span: 8 }} className="p-0">
+              <Row className="w-100 ml-0 mr-0 p-0 justify-content-center align-items-center text-center">
+                <CountdownCircleTimer
+                  style={{ width: 'auto' }}
+                  isPlaying={true}
+                  size={110}
+                  strokeWidth={8}
+                  trailColor={"#83CBDD"}
+                  duration={5 /*props.milliseconds / 1000*/}
+                  colors={"#17a2b8"}
+                >
+                  {({ remainingTime }) => {
+                    if (remainingTime > 0) {
+                      return getFormattedTimer({ remainingTime })
+                    } else {
+                      return <div className="red-alert-text">Tempo Scaduto</div>
+                    }
+                  }}
+                </CountdownCircleTimer>
+
+              </Row>
+            </Col>
+          </Row>
+        </>
+
+        {/* 
         <Timer
           initialTime={props.milliseconds}
           direction="backward"
@@ -16,7 +59,7 @@ export const QuizTimer = function (props) {
           {({ getTime }) => (
             <Card className="w-100 p-4 justify-content-center align-items-center text-center">
               {(getTime() <= 0) && (
-                <Row className="w-100 justify-content-center align-items-center text-center">
+                <Row className="w-100 ml-0 mr-0 justify-content-center align-items-center text-center">
                   <Col>
                     Tempo Scaduto!
                 </Col>
@@ -24,15 +67,68 @@ export const QuizTimer = function (props) {
               )}
               {(getTime() > 0) && (
                 <>
-                  <Row className="w-100 justify-content-center align-items-center text-center">
-                    <Col>
-                      Tempo Rimasto
-                </Col>
+                  <Row className="w-100 ml-0 mr-0 justify-content-center align-items-center text-center">
+                    <Col className="text-center p-0">
+                      <strong>Tempo Rimasto:</strong>
+                    </Col>
                   </Row>
-                  <Row className="w-100 justify-content-center align-items-center text-center">
-
-                    <Col>
-                      <Timer.Minutes /> : <Timer.Seconds />
+                  <Row className="w-100 ml-0 mr-0 p-0  justify-content-center align-items-center text-center">
+                    <Col md={{ span: 8 }} className="p-0">
+                      <Row className="w-100 ml-0 mr-0 justify-content-center align-items-center text-center">
+                        <Col md={{ span: 4 }} className="text-right p-0">
+                          <CountdownCircleTimer
+                            isPlaying
+                            size={110}
+                            duration={props.milliseconds / 1000}
+                            colors={[
+                              ['#004777', 0.33],
+                              ['#F7B801', 0.33],
+                              ['#A30000', 0.33],
+                            ]}
+                          >
+                            {({ remainingTime }) => {
+                              return remainingTime
+                            }}
+                          </CountdownCircleTimer>
+                        </Col>
+                        <Col md={{ span: 4 }} className="text-center p-0">
+                          <CountdownCircleTimer
+                            isPlaying
+                            size={110}
+                            duration={props.milliseconds / 1000}
+                            colors={[
+                              ['#004777', 0.33],
+                              ['#F7B801', 0.33],
+                              ['#A30000', 0.33],
+                            ]}
+                          >
+                            {({ remainingTime }) => {
+                              return remainingTime
+                            }}
+                          </CountdownCircleTimer>
+                        </Col>
+                        <Col md={{ span: 4 }} className="text-left p-0">
+                          <CountdownCircleTimer
+                            isPlaying
+                            size={110}
+                            duration={props.milliseconds / 1000}
+                            colors={[
+                              ['#004777', 0.33],
+                              ['#F7B801', 0.33],
+                              ['#A30000', 0.33],
+                            ]}
+                          >
+                            {({ remainingTime }) => {
+                              return remainingTime
+                            }}
+                          </CountdownCircleTimer>
+                        </Col>
+                      </Row>
+                      <Row className="w-100 ml-0 mr-0 justify-content-center align-items-center text-center">
+                        <Col md={{ span: 3 }} className="text-center p-0"> <Timer.Hours /></Col>
+                        <Col md={{ span: 3 }} className="text-center p-0" style={{ borderLeft: "1px solid grey", borderRight: "1px solid grey" }}> <Timer.Minutes /></Col>
+                        <Col md={{ span: 4 }} className="text-center p-0"> <Timer.Seconds /></Col>
+                      </Row>
                     </Col>
                   </Row>
                 </>
@@ -40,6 +136,9 @@ export const QuizTimer = function (props) {
             </Card>
           )}
         </Timer>
+*/}
+
+
       </Col>
     </Row>
   )
