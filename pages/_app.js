7d60e277942327afcc6b93a11f582773f447a552
iridion-github <antinoridavide@outlook.it>
2021-05-24 20:29:32 +0200

    24/05/2021 | Refactoring context & raging on sessionStorage

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,30 @@
 import '../styles/globals.css' //Css activation for every part of the app.
 import 'bootstrap/dist/css/bootstrap.min.css' //Css activation for every part of the app.
 import '@fortawesome/fontawesome-free/css/all.min.css' //Fontawesome activation for every part of the app.
-import { LanguageProvider } from '../context/siteLanguageContext'
+import { useEffect, useState } from 'react'
+import { AppContext } from '../context/contextLib'
 
 function MyApp({ Component, pageProps }) {
+  const [currentLang, setCurrentLang] = useState("ita")
+
+  const changeSiteLang = (newLang) => {
+    setCurrentLang(newLang)
+  }
+
+  useEffect(() => {
+    if (sessionStorage.getItem('currentLang') && sessionStorage.getItem('currentLang') !== currentLang) {
+      console.log("sessionStorage ha un currentLang settato e non coincide con quello dello state")
+      sessionStorage.setItem('currentLang', currentLang)
+      console.log("should be done updating sessionStorage")
+    }
+  })
+
   return (
-    <LanguageProvider>
+    <AppContext.Provider value={{ currentLang, changeSiteLang }}>
       <Component
         {...pageProps}
       />
-    </LanguageProvider>
+    </AppContext.Provider>
   )
 }
 
