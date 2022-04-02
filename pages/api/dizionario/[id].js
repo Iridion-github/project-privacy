bed059f9e537ce4e30b1e641a9449b8fe424db26
iridion-github <antinoridavide@outlook.it>
2022-04-02 16:30:47 +0200

    02/04/2022 | Debugging dictionary search

--- pages/api/dizionario/[id].js
+++ pages/api/dizionario/[id].js
@@ -4,32 +4,37 @@ import Dizionariorecord from "../../../models/DizionarioRecord";
 dbConnect();
 
 export default async (req, res) => {
-  const {
-    query: { searchterms },
-    method,
-    body,
-  } = req;
-  const targetDizionariorecordsIta = await Dizionariorecord.find({ ita: { $regex: searchterms, $options: "i" } }, function (err, docs) {});
-  const targetDizionariorecordsEng = await Dizionariorecord.find({ eng: { $regex: searchterms, $options: "i" } }, function (err, docs) {});
-  const targetDizionariorecordsAll = [...targetDizionariorecordsIta, ...targetDizionariorecordsEng];
+  try {
+    const {
+      query: { searchterms },
+      method,
+      body,
+    } = req;
+    const targetDizionariorecordsIta = await Dizionariorecord.find({ ita: { $regex: searchterms, $options: "i" } }, function (err, docs) {});
+    const targetDizionariorecordsEng = await Dizionariorecord.find({ eng: { $regex: searchterms, $options: "i" } }, function (err, docs) {});
+    const targetDizionariorecordsAll = [...targetDizionariorecordsIta, ...targetDizionariorecordsEng];
 
-  if (!targetDizionariorecordsAll.length) return res.status(400).json({ success: false, error: "No dictionary record found including search values!" });
+    if (!targetDizionariorecordsAll.length) return res.status(400).json({ success: false, error: "No dictionary record found including search values!" });
 
-  switch (method) {
-    case "GET":
-      try {
-        return res.status(200).json({ success: true, data: targetDizionariorecordsAll });
-      } catch (error) {
-        return res.status(400).json({ success: false, error });
-      }
+    switch (method) {
+      case "GET":
+        try {
+          return res.status(200).json({ success: true, data: targetDizionariorecordsAll });
+        } catch (error) {
+          return res.status(400).json({ success: false, error });
+        }
 
-    case "PUT":
-      return res.status(400).json({ success: false, error: "PUT not handled for this endpoint" });
+      case "PUT":
+        return res.status(400).json({ success: false, error: "PUT not handled for this endpoint" });
 
-    case "DELETE":
-      return res.status(400).json({ success: false, error: "DELETE not handled for this endpoint" });
+      case "DELETE":
+        return res.status(400).json({ success: false, error: "DELETE not handled for this endpoint" });
 
-    default:
-      res.status(400).json({ success: false, error });
+      default:
+        res.status(400).json({ success: false, error });
+    }
+  } catch (err) {
+    console.log("Server error:", err);
+    return res.status(400).json({ success: false, error: err });
   }
 };
